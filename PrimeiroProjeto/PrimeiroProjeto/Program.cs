3938afddade75af3b8459c12951812aafc88f043
Thiago Ferreira do Prado <95104754+ThiagoFerreiradoPrado@users.noreply.github.com>
2023-04-05 13:31:21 -0300

    Add files via upload
    apenas mais um Hello World pra quem começou agora  !

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriemeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
